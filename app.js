ff097a7e5575844040f4e8609a86c24695c3566c
waxir <danishdarman06@gmail.com>
2026-03-15 09:08:45 +0500

    rander task in progress

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const inputTask = document.getElementById("input-field");
+document.addEventListener("DOMContentLoaded", () => {
+  const inputTask = document.getElementById("input-field");
 
-    const addTask = document.getElementById("add-btn");
+  const addTask = document.getElementById("add-btn");
 
-    const ulList = document.getElementById("ul-list");
+  const ulList = document.getElementById("ul-list");
 
-    const tasks = JSON.parse(localStorage.stringify("tasks", tasks)) || [];
+  const tasks = JSON.parse(localStorage.stringify("tasks", tasks)) || [];
 
-    tasks.forEach((task) => randerTask(task));
+  tasks.forEach((task) => randerTask(task));
 
-    addTask.addEventListener("click", () => {
-      const taskText = inputTask.value.trim();
+  addTask.addEventListener("click", () => {
+    const taskText = inputTask.value.trim();
 
-      const newTask = {
-        id: Date.now(),
-        text: taskText,
-        completed: false,
-      };
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
 
-      tasks.push(newTask);
+    tasks.push(newTask);
+    saveTask();
+    randerTask(newTask);
+  });
+
+  function randerTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data_id", task.id);
+    li.innerHTML = `<span>${task.text}</span>
+      <button>deleted</button>
+      `;
+    li.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (e.target.tagName === "BUTTON") task.classList.add("completed");
+      task.completed = !task.completed;
       saveTask();
     });
 
-    function randerTask(task) {
-      const li = document.createElement('li')
-      li.setAttribute()
-    }
+    document.querySelector('button').addEventListener('click', ()=>{
+        
+    })
+    ulList.appendChild(li);
+  }
 
-    function saveTask() {
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-    }
-})
+  function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
