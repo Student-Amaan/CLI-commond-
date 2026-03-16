5801dab8fb6dc8431b2f9c217a78f82fd61fa38d
waxir <danishdarman06@gmail.com>
2026-03-16 20:03:59 +0500

    capatilize first latter

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function removeDuplicate(arr){
 console.log(removeDuplicate([1,2,2,3,3,5,4,6,6]))
 
 
+function capitalizeWords(sentence) {
+  return sentence
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(capitalizeWords("javascript is very powerful"));
